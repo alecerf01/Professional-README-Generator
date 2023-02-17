1e8909800c4559a3b5f5af74f5da5883214afbb6
Berserk <alexlecerf0131@gmail.com>
2023-02-17 20:04:01 +0000

    started inquirer prompt to gather input for README file structure

--- index.js
+++ index.js
@@ -15,18 +15,69 @@ const questions = [
     "What is your email address?"
 ];
 
+inquirer
+  .prompt([
+    { type: "input",
+      message: "What is the title of your project?", 
+      name: "title" },
+    {
+      type: "input",
+      message: "Give me a short description of this project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "How will you install this project?",
+      name: "install",
+    },
+    {
+        type: "input",
+        message: "Tell me about it's usage.",
+        name: "usage",
+      },
+      {
+        type: "checkbox",
+        message: "Choose your license",
+        name: "license",
+        choices: ["MIT","ISC","PUBLIC"]
+      },
+  ])
+  .then((answers) => {
+let title= `# ${answers.title}`
+let description = `## Description: \n${answers.description}`
+let install = `## Install: \n${answers.install}`
+let usage = `## Usage: \n${answers.usage}`
+
+    fs.writeFile(
+      "README.md",
+      `${title}\n${description}\n${install}\n${usage}`,
+      (err) => (err ? console.error(err) : console.log("Commit logged!"))
+    );
+  });
+
+
+// for (i = 0; i < questions.length; i++) {
+//     console.log(questions[i]);
+// }
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
 
-// function to initialize program
-function init() {
-    inquirer
-    .prompt([
-        {}
-    ])
+// fs.writeFile('README.md', process.argv[2], (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
 
-}
+// function writeToFile(fileName, data) {
+// }
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {
+//     inquirer
+//     .prompt([
+//         {}
+//     ])
+
+// }
+
+// // function call to initialize program
+// init();
+
