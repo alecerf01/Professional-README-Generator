62a9540da9071417c9e54d222d6e71787af38e19
Berserk <alexlecerf0131@gmail.com>
2023-02-17 19:02:38 +0000

    added questions for inquirer program and started on init function

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "What is the title of your project?",
+    "Give me a short description of this project.",
+    "How will you install this project?",
+    "Tell me about it's usage.",
+    "What licence are you using?",
+    "Who contributed to this project? Input there github usernames.",
+    "What is your GitHub username?",
+    "What is your email address?"
 ];
 
 // function to write README file
@@ -14,6 +21,10 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer
+    .prompt([
+        {}
+    ])
 
 }
 
